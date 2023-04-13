833ad0802b0f93e6f65a75ba367aaa3b83862000
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-13 11:09:49 +0300

    added the login api in the backen

--- fastapi-backend/app/controllers/auth_controller.py
+++ fastapi-backend/app/controllers/auth_controller.py
@@ -1,10 +1,26 @@
 import jwt
+from models.userModel import User
+from configs.config import SECRET_KEY
 
+
+async def register(request):
+    data = request.json
+    name = data["name"]
+    email = data["email"]
+    password = data["password"]
+    role = data.get("role", "user")
+
+    existing_user = User.objects(email=email).first()
+    if existing_user:
+        return {"message" : "Eamil already exists"}, 409
+    user = User(name=name, email=email, password=password, role=role)
+    user.save()
+
+    token = jwt.encode({"id":str(user.id), "email": user.email}, SECRET_KEY, algorithm="HS256")
+    new_user = user.to_mongo().to_dict()
+    del new_user["_id"]
+    del new_user["password"]
+    return {"user": new_user, "token": token}, 201
 
 async def login():
     print("hello login")
-
-
-async def register():
-    print("hello register")
-
