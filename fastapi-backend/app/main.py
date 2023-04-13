20ba8aa72df8c1cec36f32e88304954b87c0c4ae
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-13 10:08:08 +0300

    added the auth router import

--- fastapi-backend/app/main.py
+++ fastapi-backend/app/main.py
@@ -1,14 +1,11 @@
-# from fastapi import APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Union
 from fastapi import FastAPI
 from configs.db_config import db_connection_keep_on
-from models.userModel import User
-import json
+# from models.userModel import User
+# import json
 
-from app.routes.auth
-
-app = FastAPI()
+from app.routes.auth_route import router as auth_router
 
 db = db_connection_keep_on()
 
