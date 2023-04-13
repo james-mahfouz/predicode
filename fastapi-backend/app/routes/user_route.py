f7a2044ad2b4d78e5b28d60d1153b3017aba5031
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-13 22:47:37 +0300

    added route for the upload file api

--- fastapi-backend/app/routes/user_route.py
+++ fastapi-backend/app/routes/user_route.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from controllers.user_controller import get_files
+from controllers.user_controller import get_files, upload_file
 from middlewares.authMiddleware import get_current_user
 from fastapi import Depends
 from models.userModel import User
+from request_models.userRequest import FileRequest
 
 router = APIRouter()
 
@@ -10,3 +11,8 @@ router = APIRouter()
 @router.get("/get_files")
 async def files(user: User = Depends(get_current_user)):
     return get_files(user)
+
+
+@router.post("/upload_files")
+async def upload(user: User = Depends(get_current_user)):
+    return await upload_file(user=user, request=FileRequest)
