4ff5b5d81c282e1c878bf86346283f434fbbd32f
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-13 21:23:03 +0300

    finished the upload file api
    fixed git problem
    
    fixing

--- fastapi-backend/app/controllers/user_controller.py
+++ fastapi-backend/app/controllers/user_controller.py
@@ -1,3 +1,22 @@
+from uuid import uuid4
+import boto3
+from models.fileModel import File
+
+
 def get_files(user):
     print(f"hello {user.name}")
     return {"message": "welcome"}
+
+
+def upload_file(request, user):
+    filename = f"{uuid4()}-{request.filename}"
+
+    s3 = boto3.client("s3")
+    s3.upload_filobj(request.file, "predicode_files", filename)
+
+    file = File(filename=request.filename, by_user=user.name, path=f"s3://predicode_files/{filename}")
+    file.save()
+
+    user.files.append(file)
+    
+    return {"message": "File created successfully"}
--- fastapi-backend/app/models/fileModel.py
+++ fastapi-backend/app/models/fileModel.py
@@ -3,4 +3,5 @@ from mongoengine import Document, StringField, ReferenceField
 
 class File(Document):
     name = StringField(max_length=100, required=True)
+    path = StringField(required=True)
     by_user = StringField(required=True)
