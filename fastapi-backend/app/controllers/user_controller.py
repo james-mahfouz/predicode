ce5b4ab00e32bd06231f4d375ce97c16b40c404a
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-14 11:32:09 +0300

    added the get files api

--- fastapi-backend/app/controllers/user_controller.py
+++ fastapi-backend/app/controllers/user_controller.py
@@ -1,14 +1,23 @@
 from models.fileModel import File
+from models.userModel import User
 import shutil
 
 
 def get_files(user):
-    print(f"hello {user.id}")
-    return {"message": "welcome"}
+    # user1 = User.objects(id=user.id).first()
+    # user1.files = File.objects.all()
+    # user1.populate("files")
+    for file in user.files:
+        print(f"File Name: {str(file)}")
+        print(f"File Path: {file.path}")
+    return {"files": "files"}
 
 
 def upload_file(file, user):
     save_path = f"public/{file.filename}"
+    if File.objects(name=file.filename).first():
+        return {"message": "file with this name already exist"}
+
     with open(save_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
