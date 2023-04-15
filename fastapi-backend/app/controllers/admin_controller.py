1c1a12bc379645a3b2479df56f8579c8998d3838
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-15 12:17:35 +0300

    added the the get files function inthe admin controller

--- fastapi-backend/app/controllers/admin_controller.py
+++ fastapi-backend/app/controllers/admin_controller.py
@@ -1,2 +1,17 @@
+from models.fileModel import File
+import json
+
+
 def get_files(user):
-    print("hello world")
+    files = File.objects.all()
+    files_dict = {"files": []}
+    for file in files:
+        files_dict["files"].append({
+            "id": str(file.id),
+            "name": file.name,
+            "path": file.path,
+            "by_user": file.by_user
+        })
+    files_json = json.dumps(files_dict)
+
+    return files_json
