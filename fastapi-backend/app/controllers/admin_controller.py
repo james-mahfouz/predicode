d8d3ca4ae964d5ce037aae3c26e754b07fcccb9b
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-15 12:49:54 +0300

    finished the get files in the admin controller

--- fastapi-backend/app/controllers/admin_controller.py
+++ fastapi-backend/app/controllers/admin_controller.py
@@ -1,17 +1,43 @@
 from models.fileModel import File
-import json
+from models.userModel import User
+from fastapi.responses import JSONResponse
+from bson import ObjectId
 
 
 def get_files(user):
     files = File.objects.all()
-    files_dict = {"files": []}
+    files_list = []
     for file in files:
-        files_dict["files"].append({
-            "id": str(file.id),
-            "name": file.name,
-            "path": file.path,
-            "by_user": file.by_user
-        })
-    files_json = json.dumps(files_dict)
+        file_dict = file.to_mongo().to_dict()
+        # Convert ObjectId to string
+        file_dict["_id"] = str(file_dict["_id"])
+        files_list.append(file_dict)
 
-    return files_json
+    return JSONResponse(content={"files": files_list})
+#     files = File.objects.all()
+#     files_dict = {"files": []}
+#     for file in files:
+#         files_dict["files"].append({
+#             "id": str(file.id),
+#             "name": file.name,
+#             "path": file.path,
+#             "by_user": file.by_user
+#         })
+#     files_json = json.dumps(files_dict)
+#     files_json = json.dumps(files_list)
+#     return JSONResponse(content={"files": files_list})
+
+
+def get_users(user):
+    users = User.objects.all()
+#     users_dict = {"files": []}
+#     for user in users:
+#         files_dict["files"].append({
+#             "id": str(file.id),
+#             "name": file.name,
+#             "path": file.path,
+#             "by_user": file.by_user
+#         })
+#     files_json = json.dumps(files_dict)
+#
+#     return {"files": files_json}
--- fastapi-backend/app/routes/admin_route.py
+++ fastapi-backend/app/routes/admin_route.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, UploadFile, File
-from controllers.admin_controller import get_files
+from fastapi import APIRouter
+from controllers.admin_controller import get_files, get_users
 from middlewares.adminMiddleware import get_current_user
 from fastapi import Depends
 from models.userModel import User
@@ -13,6 +13,6 @@ async def files(user: User = Depends(get_current_user)):
     return get_files(user)
 
 
-# @router.post("/upload_files")
-# async def upload(file: UploadFile = File(...), user: User = Depends(get_current_user)):
-#     return upload_file(file, user=user)
+@router.post("/get_users")
+async def users(user: User = Depends(get_current_user)):
+    return get_users(user=user)
