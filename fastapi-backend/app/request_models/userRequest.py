9b41d5e1a130228cc9c96d156b1fafc96959cf3f
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-15 11:45:07 +0300

    adjusted the upload file funciton

--- fastapi-backend/app/request_models/userRequest.py
+++ fastapi-backend/app/request_models/userRequest.py
@@ -5,29 +5,8 @@ from io import BytesIO
 from typing import List
 
 
-
 class FileRequest(BaseModel):
     file: UploadFile = File(...)
     name: str = Form(...)
 
 
-class SubFile(BaseModel):
-    filename: str
-    contents: bytes
-
-
-class UploadFolder(BaseModel):
-    folder: UploadFile
-    sub_files: List[SubFile]
-
-    def __init__(self, folder: UploadFile):
-        self.folder = folder
-        self.sub_files = []
-        with zipfile.ZipFile(folder.file, 'r') as zip_ref:
-            for filename in zip_ref.namelist():
-                with zip_ref.open(filename) as sub_file:
-                    sub_file_object = SubFile(
-                        filename=filename,
-                        contents=sub_file.read(),
-                    )
-                    self.sub_files.append(sub_file_object)
