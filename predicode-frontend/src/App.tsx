1f65993effeee645b035c78d583d4e7cbb90a68d
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-15 19:14:10 +0300

    started the frontend

--- predicode-frontend/src/App.tsx
+++ predicode-frontend/src/App.tsx
@@ -1,56 +1,33 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import { FileUpload } from 'primereact/fileupload';
-
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
-import 'primereact/resources/primereact.min.css'; //core css
-import 'primeicons/primeicons.css'; //icons
-import 'primeflex/primeflex.css'; // flex
 
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [profilePicture, setProfilePicture] = useState(null);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(name, email, phone)
-  };
+  const [count, setCount] = useState(0)
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="p-field">
-        <label htmlFor="name">Name</label>
-        <InputText id="name" value={name} onChange={(e) => setName(e.target.value)} />
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <div className="p-field">
-        <label htmlFor="email">Email</label>
-        <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
       </div>
-      <div className="p-field">
-        <label htmlFor="phone">Phone</label>
-        <InputText id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
-      </div>
-      <div className="p-field">
-        <label htmlFor="profilePicture">Profile Picture</label>
-        <FileUpload
-          id="profilePicture"
-          name="profilePicture"
-          accept="image/*"
-          maxFileSize={1000000}
-          chooseLabel="Choose"
-          uploadLabel="Upload"
-          cancelLabel="Cancel"
-          mode="basic"
-          onUpload={(event) => setProfilePicture(event.files[0])}
-        />
-      </div>
-      <Button label="Save" type="submit" />
-    </form>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
   )
 }
 
