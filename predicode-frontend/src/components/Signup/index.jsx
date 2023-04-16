7e952abb21901296821d3fd54bfe6fb0c86fea20
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-16 16:05:11 +0300

    added form validation

--- predicode-frontend/src/components/Signup/index.jsx
+++ predicode-frontend/src/components/Signup/index.jsx
@@ -4,72 +4,107 @@ import axios from "axios";
 import "../Login/index.css";
 import logo from "../../assets/logo.png";
 
-
 function Signup() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState('');
-    const [emailError, setEmailError] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);
-    const navigate = useNavigate();
-    const apiUrl = process.env.API_URL;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const navigate = useNavigate();
+  const apiUrl = process.env.API_URL;
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        console.log(name, email, password)
-        const data = {
-            "name": name,
-            "email": email,
-            "password": password
-        }
+    if (!name) {
+      setNameError("Wrong Name ");
+      return;
+    }
 
-        try {
-            const response = await axios.post('http://localhost:4000/auth/register', data);
-            localStorage.setItem('token', response.data.token);
-            navigate("/")
-        } catch (error) {
-            console.log(error);
-        }
+    if (!email) {
+      setEmailError("Please enter your email.");
+      return;
+    }
+
+    if (!password) {
+      setPasswordError("Please enter your password.");
+      return;
+    }
+
+    const data = {
+      name: name,
+      email: email,
+      password: password,
     };
 
-    return (
-        <div className="signin-wrapper">
-            <div className="signin-logo">
-                <img src={logo} alt=""></img>
-            </div>
-            <div className="wrapper">
-                <div className="title">
-                    <h2>Sign-Up</h2>
-                </div>
+    try {
+      const response = await axios.post(apiUrl + "auth/register", data);
+      localStorage.setItem("token", response.data.token);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-                <div className="form">
-                    <div className="inputfield">
-                        <label>Name</label>
-                        <input type="text" className="register_input" value={name} onChange={(e) => setName(e.target.value)}></input>
-                    </div>
-
-
-                    <div className="inputfield">
-                        <label>Email</label>
-                        <input type="email" className="register_input" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-                    </div>
-
-                    <div className="inputfield">
-                        <label>Password</label>
-                        <input type="password" className="register_input" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-                    </div>
-
-                    <div className="inputfield">
-                        <input type="submit" value="Register" className="btn" onClick={handleSubmit}></input>
-                    </div>
-
-                    <p>Have an account? <a href="/login">Login</a></p>
-                </div>
-            </div>
+  return (
+    <div className="signin-wrapper">
+      <div className="signin-logo">
+        <img src={logo} alt=""></img>
+      </div>
+      <div className="wrapper">
+        <div className="title">
+          <h2>Sign-Up</h2>
         </div>
-    );
+
+        <div className="form">
+          <div className="inputfield">
+            <label>Name</label>
+            <input
+              type="text"
+              className="register_input"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </div>
+
+          <div className="inputfield">
+            <label>Email</label>
+            <input
+              type="email"
+              className="register_input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </div>
+
+          <div className="inputfield">
+            <label>Password</label>
+            <input
+              type="password"
+              className="register_input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </div>
+
+          <div className="inputfield">
+            <input
+              type="submit"
+              value="Register"
+              className="btn"
+              onClick={handleSubmit}
+            ></input>
+          </div>
+
+          <p>
+            Have an account? <a href="/login">Login</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
