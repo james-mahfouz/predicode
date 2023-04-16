ccc3a42e0bae3846ca462080a7cbfde4aac65a92
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-16 13:09:30 +0300

    added the --save-dev package

--- predicode-frontend/src/components/Login/index.jsx
+++ predicode-frontend/src/components/Login/index.jsx
@@ -9,7 +9,7 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-    const apiUrl = process.env.API_URL;
+    const apiUrl = process.env.API_URL
 
     const handleSubmit = async (event) => {
         event.preventDefault();
--- predicode-frontend/vite.config.js
+++ predicode-frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import replace from '@rollup/plugin-replace';
+import dotenv from 'dotenv';
+
+dotenv.config(); // load environment variables from .env file
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    replace({
+      'process.env.API_URL': JSON.stringify(process.env.API_URL),
+      preventAssignment: true,
+    }),
+  ],
 })
