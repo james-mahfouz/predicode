73106840c2e16bac8f7792287205e0a8568ef09d
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-20 13:42:28 +0300

    added the right section in the admin panel

--- predicode-frontend/src/components/Admin/index.jsx
+++ predicode-frontend/src/components/Admin/index.jsx
@@ -1,5 +1,24 @@
+import logo from "../../assets/logo.png";
+import "./index.css";
+
 const Admin = () => {
-  return <div>hello Admin</div>;
+  return (
+    <div className="admin-body">
+      <section className="right">
+        <div className="admin-logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="options">
+          <div className="option">
+            <h4>Display Users</h4>
+          </div>
+          <div className="option">
+            <h4>Files Uploaded</h4>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Admin;
