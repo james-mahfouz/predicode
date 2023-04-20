92dba382d593b08bea8ac54d0962f0ab0f2ebe9c
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-20 12:00:33 +0300

    added the admin page

--- predicode-frontend/src/App.jsx
+++ predicode-frontend/src/App.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import LandingPage from './pages/LandingPage';
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import LandingPage from "./pages/LandingPage";
+import AdminPage from "./pages/AdminPage";
 
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
-import 'primereact/resources/primereact.min.css'; //core css
-import 'primeicons/primeicons.css'; //icons
-import 'primeflex/primeflex.css'; // flex
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import "primeflex/primeflex.css"; // flex
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <Router >
+    <Router>
       <div className="body">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/admin" element={<AdminPage />} />
           <Route path="*" element={() => <div>404</div>} />
         </Routes>
       </div>
     </Router>
-
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ predicode-frontend/src/pages/AdminPage/index.jsx
@@ -0,0 +1,10 @@
+import Landing from "../../components/Landing";
+
+const AdminPage = () => {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+};
+export default AdminPage;
