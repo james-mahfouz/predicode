9abd0ea2c3c053cf789b463cfa36a4d6a9df1f35
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-20 18:56:14 +0300

    adjusted the get_files function to get the files and display them

--- predicode-frontend/src/components/DisplayFiles/index.jsx
+++ predicode-frontend/src/components/DisplayFiles/index.jsx
@@ -3,23 +3,22 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-import { Dropdown } from "primereact/dropdown";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 
-const DisplayUsers = () => {
-  const [users, setUsers] = useState([]);
+const DisplayFiles = () => {
+  const [files, setFiles] = useState([]);
   const apiUrl = process.env.API_URL;
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const response = await axios.get(apiUrl + "admin/get_users", {
+        const response = await axios.get(apiUrl + "admin/get_files", {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
         });
-        console.log(response.data.users[0].files);
-        setUsers(response.data.users);
+        console.log(response);
+        setFiles(response.data.files);
       } catch (e) {
         console.log(e);
       }
@@ -32,29 +31,33 @@ const DisplayUsers = () => {
       <h1>Display Users</h1>
       <div className="card">
         <DataTable
-          value={users}
+          value={files}
           scrollable
           scrollHeight="400px"
           virtualScrollerOptions={{ itemSize: 46 }}
           tableStyle={{ minWidth: "50rem" }}
         >
           <Column field="_id" header="Id" style={{ width: "20%" }}></Column>
-          <Column field="name" header="Name" style={{ width: "20%" }}></Column>
           <Column
-            field="email"
-            header="email"
+            field="name"
+            header="File Name"
             style={{ width: "20%" }}
           ></Column>
           <Column
+            field="by_user"
+            header="File Owner"
+            style={{ width: "20%" }}
+          ></Column>
+          {/* <Column
             header="Files nb."
             style={{ width: "20%" }}
             body={(rowData) => rowData.files.length}
-          ></Column>
-          <Column
+          ></Column> */}
+          {/* <Column
             header="Files"
             style={{ width: "20%" }}
             body={(rowData) => <FilesColumn rowData={rowData} />}
-          />
+          /> */}
         </DataTable>
       </div>
     </div>
