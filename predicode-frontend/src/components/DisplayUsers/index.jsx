a940c9164ecf5920ce53334a6aac462a5fe9e596
James Mahfouz <jamesmahfouz0@gmail.com>
2023-04-20 18:08:58 +0300

    added the option to display files uploaded by a user as a dropdown list

--- predicode-frontend/src/components/DisplayUsers/index.jsx
+++ predicode-frontend/src/components/DisplayUsers/index.jsx
@@ -3,28 +3,28 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+import { Dropdown } from "primereact/dropdown";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 
 const DisplayUsers = () => {
-  const [files, setFiles] = useState([]);
+  const [users, setUsers] = useState([]);
   const apiUrl = process.env.API_URL;
   useEffect(() => {
-    const getFiles = async () => {
-      console.log("enterd function");
+    const getUsers = async () => {
       try {
-        const response = await axios.get(apiUrl + "user/get_files", {
+        const response = await axios.get(apiUrl + "admin/get_users", {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
         });
-        console.log(response);
-        setFiles(response.data.files);
+        console.log(response.data.users[0].files);
+        setUsers(response.data.users);
       } catch (e) {
         console.log(e);
       }
     };
-    getFiles();
+    getUsers();
   }, []);
 
   return (
@@ -32,28 +32,53 @@ const DisplayUsers = () => {
       <h1>Display Users</h1>
       <div className="card">
         <DataTable
-          value={files}
+          value={users}
           scrollable
           scrollHeight="400px"
           virtualScrollerOptions={{ itemSize: 46 }}
           tableStyle={{ minWidth: "50rem" }}
         >
-          <Column field="id" header="Id" style={{ width: "20%" }}></Column>
-          <Column field="vin" header="Vin" style={{ width: "20%" }}></Column>
-          <Column field="year" header="Year" style={{ width: "20%" }}></Column>
+          <Column field="_id" header="Id" style={{ width: "20%" }}></Column>
+          <Column field="name" header="Name" style={{ width: "20%" }}></Column>
           <Column
-            field="brand"
-            header="Brand"
+            field="email"
+            header="email"
             style={{ width: "20%" }}
           ></Column>
           <Column
-            field="color"
-            header="Color"
+            header="Files"
             style={{ width: "20%" }}
+            body={(rowData) => rowData.files.length}
           ></Column>
+          <Column
+            header="Files"
+            style={{ width: "20%" }}
+            body={(rowData) => <FilesColumn rowData={rowData} />}
+          />
+          ;
         </DataTable>
       </div>
     </div>
   );
 };
 export default DisplayUsers;
+
+function FilesColumn(props) {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const fileOptions = props.rowData.files.map((file) => ({
+    label: file.name,
+    value: file.id,
+  }));
+
+  return (
+    <div>
+      <Dropdown
+        options={fileOptions}
+        value={selectedFile}
+        onChange={(e) => setSelectedFile(e.value)}
+        placeholder="View all files"
+      />
+    </div>
+  );
+}
