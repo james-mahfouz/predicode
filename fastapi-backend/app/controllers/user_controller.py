69f0accf03336f6e75cc4fc6fcbfda499810b33f
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-01 15:42:57 +0300

    backend: removed the MACOSX folder created automaatically

--- fastapi-backend/app/controllers/user_controller.py
+++ fastapi-backend/app/controllers/user_controller.py
@@ -35,37 +35,51 @@ def get_files(user):
 
 
 def upload_file(file, user):
-    print(file.content_rating, file.category, file.price)
-    if file.content_type == "data:application/zip;base64":
-        decoded_data = base64.b64decode(file.data)
+    try:
+        print(file.content_rating, file.category, file.price)
+        if file.content_type == "data:application/zip;base64":
+            decoded_data = base64.b64decode(file.data)
 
-        temp_file_path = file.name
-        with open(temp_file_path, 'wb') as f:
-            f.write(decoded_data)
+            temp_file_path = file.name
+            with open(temp_file_path, 'wb') as f:
+                f.write(decoded_data)
 
-        with zipfile.ZipFile(temp_file_path, 'r') as zip_ref:
-            zip_ref.extractall()
+            with zipfile.ZipFile(temp_file_path, 'r') as zip_ref:
+                zip_ref.extractall()
 
-        unzipped_file_name = os.path.splitext(file.name)[0]
+            unzipped_file_name = os.path.splitext(file.name)[0]
+            print(unzipped_file_name)
+            if File.objects(name=unzipped_file_name).first():
+                unzipped_file_name += "_copy"
+                if File.objects(name=unzipped_file_name).first():
 
-        save_path = os.path.join('public', unzipped_file_name)
+                    for i in range(1, 100000):
+                        if not File.objects(name=unzipped_file_name+str(i)).first():
+                            unzipped_file_name + str(i)
+                            break
 
-        if File.objects(name=unzipped_file_name).first():
-            unzipped_file_name += "_copy"
+            save_path = os.path.join('public', unzipped_file_name)
 
-        save_path_copy = os.path.join('public', unzipped_file_name)
+            folder_name = os.path.commonprefix(zip_ref.namelist()).rstrip('/')
 
-        shutil.move(unzipped_file_name, save_path)
+            shutil.move(folder_name, save_path)
 
-        uploaded_file = File(name=unzipped_file_name, by_user=user.name, path=save_path)
-        uploaded_file.save()
+            uploaded_file = File(name=unzipped_file_name, by_user=user.name, path=save_path)
+            uploaded_file.save()
 
-        user.files.append(uploaded_file)
-        user.save()
+            user.files.append(uploaded_file)
+            user.save()
 
-        os.remove(temp_file_path)
-        return {
-            "message": "File created successfully",
-            "file_path": save_path
-        }
+            os.remove(temp_file_path)
 
+            macosx_folder = os.path.join(save_path, '__MACOSX')
+            if os.path.exists(macosx_folder):
+                shutil.rmtree(macosx_folder)
+
+            return {
+                "message": "File created successfully",
+                "file_path": save_path
+            }
+
+    except Exception as e:
+        print(e)
--- fastapi-backend/app/reactjs-session2/src/components/TodoList/TodoItemList/Item/index.js
+++
@@ -1,8 +0,0 @@
-const Item = ({ todo }) => {
-
-  return (<li>
-    {todo} <button className="button">remove</button>
-  </li>)
-}
-
-export default Item;
