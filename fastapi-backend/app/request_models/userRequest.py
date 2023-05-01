98527b2d122c1a6f9cb9955d2252434912c8e5bc
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-01 13:44:13 +0300

    added the category and content rating receival in the request model

--- fastapi-backend/app/request_models/userRequest.py
+++ fastapi-backend/app/request_models/userRequest.py
@@ -8,11 +8,8 @@ from typing import List
 class FileRequest(BaseModel):
     data: str
     content_type: str
-    appName: str
     price: float
-
-
-    # file: UploadFile = File(...)
-    # name: str = Form(...)
+    category: str
+    content_rating: str
 
 
