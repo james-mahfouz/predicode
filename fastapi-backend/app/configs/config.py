fbd93fab8c42833c63f5fad67610d979d537be4e
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-04 23:16:06 +0300

    backend: removed config file from being tracked

--- fastapi-backend/app/configs/config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = 'HELLOWORLD12345678987654321'
-DB_NAME = "predicode_db"
-DB_HOST = "localhost"
-DB_URL = 'mongodb://localhost:27017/predicode_db'
-DB_PORT = 27017
-OPEN_AI_KEY = "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"
