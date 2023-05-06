dcfcef19642270a54c7a29abf6f296dbf9c510ae
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-06 09:35:23 +0300

    backend: added a an if condition if the path is a file

--- fastapi-backend/app/controllers/admin_controller.py
+++ fastapi-backend/app/controllers/admin_controller.py
@@ -8,35 +8,47 @@ def get_files(user):
     files = File.objects.all()
     files_list = []
     for file in files:
+        get_directory_contents(file.path)
         file_dict = file.to_mongo().to_dict()
         file_dict["_id"] = str(file_dict["_id"])
         files_list.append(file_dict)
 
-    return JSONResponse(content={
-        "files": files_list,
-        "admin_name": user.name
-    })
+    # return JSONResponse(content={
+    #     "files": files_list,
+    #     "admin_name": user.name
+    # })
 
 
 def get_directory_contents(path):
     contents = []
-    for entry in os.scandir(path):
-        if entry.is_file():
-            contents.append({
-                'type': 'file',
-                'name': entry.name,
-                'path': entry.path,
-                'onclick': f"window.open('{entry.path}')"
-            })
-        elif entry.is_dir():
-            subdir_contents = get_directory_contents(entry.path)
-            contents.append({
-                'type': 'folder',
-                'name': entry.name,
-                'path': entry.path,
-                'contents': subdir_contents
-            })
-    return contents
+    if os.path.isfile(path):
+        print(path)
+        basename = os.path.basename(path)
+        contents.append({
+            'type': 'file',
+            'name': basename,
+            'path': path,
+            'onclick': f"window.open('{path}')"
+        })
+    else:
+        for entry in os.scandir(path):
+            if entry.is_file():
+                print(entry.name)
+                contents.append({
+                    'type': 'file',
+                    'name': entry.name,
+                    'path': entry.path,
+                    'onclick': f"window.open('{entry.path}')"
+                })
+            elif entry.is_dir():
+                subdir_contents = get_directory_contents(entry.path)
+                contents.append({
+                    'type': 'folder',
+                    'name': entry.name,
+                    'path': entry.path,
+                    'contents': subdir_contents
+                })
+        # return contents
 
 
 def get_users(user):
