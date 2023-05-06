4a4eea6e6ac2e75d409062c823795713220be33f
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-06 17:15:32 +0300

    frontend: added the function to adjust the data gotten from the backend

--- predicode-frontend/src/components/DisplayFiles/index.jsx
+++ predicode-frontend/src/components/DisplayFiles/index.jsx
@@ -1,6 +1,6 @@
 import "../Admin/index.css";
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -11,6 +11,7 @@ import { SlideMenu } from "primereact/slidemenu";
 
 const DisplayFiles = ({ onAdminNameChange }) => {
   const [files, setFiles] = useState([]);
+  const menu = useRef(null);
   const apiUrl = process.env.API_URL;
   const navigate = useNavigate();
 
@@ -22,8 +23,10 @@ const DisplayFiles = ({ onAdminNameChange }) => {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
         });
-        console.log(response.data);
+        // console.log(response.data.files);
+
         setFiles(response.data.files);
+        // console.log(response.data.files[1].content);
         localStorage.setItem("admin_name", response.data.admin_name);
       } catch (e) {
         if (e.response.data.detail.access === "denied") {
@@ -34,10 +37,39 @@ const DisplayFiles = ({ onAdminNameChange }) => {
     getUsers();
   }, []);
 
+  function adjustDataForSlideMenu(files) {
+    const menuItems = [];
+
+    // Loop through each file object in the array
+    for (let file of files) {
+      // Create a new menu item object
+      console.log(file.name);
+      const menuItem = {
+        label: file.name,
+        icon: file.type === "folder" ? "pi pi-folder" : "pi pi-file",
+      };
+
+      // If the file is a folder, recursively add its contents as subitems
+      if (file.type === "folder") {
+        menuItem.items = adjustDataForSlideMenu(file.items);
+      } else if (file.type === "file") {
+        // Otherwise, set the onclick function to open the file
+        menuItem.command = () => window.open(`${apiUrl}${file.path}`);
+      }
+
+      // Add the new menu item to the array of menu items
+      menuItems.push(menuItem);
+    }
+
+    return menuItems;
+  }
+
   const viewFile = (path) => {
     window.open(`${apiUrl}${path}`);
   };
 
+  const hello = adjustDataForSlideMenu(files);
+  console.log(hello);
   return (
     <div className="display-users">
       <h1>Files</h1>
@@ -77,23 +109,24 @@ const DisplayFiles = ({ onAdminNameChange }) => {
             //     onClick={() => viewFile(rowData.path)}
             //   />
             // )}
-            // body={(rowData) => (
-            //   <>
-            //     <SlideMenu
-            //       ref={menu}
-            //       model={items}
-            //       popup
-            //       viewportHeight={220}
-            //       menuWidth={175}
-            //     ></SlideMenu>
-            //     <Button
-            //       type="button"
-            //       icon="pi pi-bars"
-            //       label="Show"
-            //       onClick={(event) => menu.current.toggle(event)}
-            //     ></Button>
-            //   </>
-            // )}
+            body={(rowData) => (
+              <>
+                {/* {console.log("rowData:", rowData.content)} */}
+                <SlideMenu
+                  ref={menu}
+                  model={hello}
+                  popup
+                  viewportHeight={220}
+                  menuWidth={175}
+                ></SlideMenu>
+                <Button
+                  type="button"
+                  icon="pi pi-bars"
+                  label="Show"
+                  onClick={(event) => menu.current.toggle(event)}
+                ></Button>
+              </>
+            )}
             headerStyle={{ backgroundColor: "#714DF4", color: "white" }}
           />
         </DataTable>
