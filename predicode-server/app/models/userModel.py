f13c9e3a463304cf3d2b92ef3692eb38983826c4
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-07 12:34:42 +0300

    desktop: started the desktop app using electron

--- predicode-server/app/models/userModel.py
+++ predicode-server/app/models/userModel.py
@@ -6,6 +6,6 @@ from models.fileModel import File
 class User(Document):
     name = StringField(max_length=100, required=True)
     email = EmailField(unique=True, required=True)
-    password = StringField(required=True)
+    password = StringField()
     role = StringField(choices=["user", "admin"], default="user")
     files = ListField(ReferenceField(File))
