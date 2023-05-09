217fcd2cc31a2472beb27f86442d0895d8393de5
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-09 16:42:37 +0300

    backend: added the add history function

--- predicode-server/app/controllers/user_controller/add_history.py
+++ predicode-server/app/controllers/user_controller/add_history.py
@@ -1,19 +1,9 @@
 from models.historyModel import History
 
 
-def add_history(name, category, content_rating,
+def add_history(user, name, category, content_rating,
                 price, size, rating, maintainability):
     history = History(name=name, category=category, content_rating=content_rating,
-                          price=price, size=size, rating=rating,maintainability=maintainability)
+                      price=price, size=size, rating=rating, maintainability=maintainability)
+    user.history.append(history)
 
-
-
-
-    # name = StringField(max_length=100, required=True)
-    # category = StringField(required=True)
-    # content_rating = StringField(required=True)
-    # price = FloatField(required=True)
-    # size = FloatField(required=True)
-    # rating = FloatField(required=True)
-    # maintainability = StringField(required=True)
-    # date_created = DateTimeField(required=True)
