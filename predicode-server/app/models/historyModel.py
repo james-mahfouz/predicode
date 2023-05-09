cd29b6860131179bf43ca0a94d8a4adb7ca9c6b5
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-09 16:04:50 +0300

    backend: added the date created to the history model

--- predicode-server/app/models/historyModel.py
+++ predicode-server/app/models/historyModel.py
@@ -1,13 +1,13 @@
-from mongoengine import Document, StringField, FloatField
+from mongoengine import Document, StringField, FloatField, DateTimeField
 
 
-class File(Document):
+class History(Document):
     name = StringField(max_length=100, required=True)
-    path = StringField(required=True)
-    by_user = StringField(required=True)
     category = StringField(required=True)
     content_rating = StringField(required=True)
     price = FloatField(required=True)
     size = FloatField(required=True)
     rating = FloatField(required=True)
     maintainability = StringField(required=True)
+    date_created = DateTimeField(required=True)
+
