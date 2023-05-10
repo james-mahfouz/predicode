548afff6615d9eb1b22f1f965d31ab469f3ca1b3
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-10 18:39:06 +0300

    frontend: finished the profile picture on hover

--- predicode-frontend/src/components/UserProfile/index.jsx
+++ predicode-frontend/src/components/UserProfile/index.jsx
@@ -1,3 +1,4 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../Admin/index.css";
 import React from "react";
 import { useState, useEffect } from "react";
@@ -14,9 +15,24 @@ const UserProfile = () => {
   const [email, setEmail] = useState("email@gmail.com");
   const [profilePic, setProfilePic] = useState(no_pp);
   const [editable, setEditable] = useState(false);
-
   const [loading, setLoading] = useState(false);
 
+  const apiUrl = process.env.API_URL;
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await axios.get(apiUrl + "user/get_history", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        setHistory(response.data.history);
+        localStorage.setItem("user_name", response.data.user_name);
+      } catch (e) {}
+    };
+    getUser();
+  }, []);
   const load = () => {
     setLoading(true);
 
@@ -28,22 +44,6 @@ const UserProfile = () => {
     e.preventDefault();
     console.log(email, name);
   };
-  const apiUrl = process.env.API_URL;
-  useEffect(() => {
-    const getUsers = async () => {
-      try {
-        const response = await axios.get(apiUrl + "user/get_history", {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        });
-        setHistory(response.data.history);
-        localStorage.setItem("user_name", response.data.user_name);
-      } catch (e) {}
-    };
-    getUsers();
-  }, []);
-
   const handleEditClick = () => {
     setEditable(true);
   };
@@ -53,11 +53,38 @@ const UserProfile = () => {
     // handle save logic here
   };
 
+  const handleAvatarClick = () => {
+    document.getElementById("fileInput").click();
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setProfilePic(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
     <div className="users_info">
       <h1>Edit Profile</h1>
       <div className="profile-pic">
-        <Avatar src={profilePic} name={name} size="150" round={true} />
+        <Avatar
+          src={profilePic}
+          name={name}
+          size="150"
+          round={true}
+          onClick={handleAvatarClick}
+          className="user_pp"
+        />
+
+        <input
+          type="file"
+          id="fileInput"
+          style={{ display: "none" }}
+          onChange={handleFileChange}
+        />
       </div>
       <form onSubmit={handleSubmit}>
         <>
