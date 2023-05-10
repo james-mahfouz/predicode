a4934353880a825e8599abc12df605dfa0ed2ec9
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-11 01:31:54 +0300

    backend: added the route for get history

--- predicode-server/app/routes/admin_route.py
+++ predicode-server/app/routes/admin_route.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from controllers.admin_controller import get_files, get_users
+from controllers.admin_controller import get_files, get_users, get_history
 from middlewares.adminMiddleware import get_current_user
 from fastapi import Depends
 from models.userModel import User
@@ -15,3 +15,8 @@ async def files(user: User = Depends(get_current_user)):
 @router.get("/get_users")
 async def users(user: User = Depends(get_current_user)):
     return get_users(user=user)
+
+
+@router.get("/get_history")
+async def history(user: User = Depends(get_current_user)):
+    return get_history(user=user)
